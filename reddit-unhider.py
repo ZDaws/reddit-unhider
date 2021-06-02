73b40182fd090cc64c8f8ae07381171e03887892
Zach Dawson <zach.a.dawson@gmail.com>
2021-06-01 21:24:48 -0500

    Upgrade script to work with new version of PRAW
    Also make the following tweaks
    - Use .env file for PRAW config
    - Remove "days_to_keep" functionality

--- reddit-unhider.py
+++ reddit-unhider.py
@@ -1,34 +1,27 @@
-import argparse
+import os
+
 import praw
-import time
+from dotenv import load_dotenv
 
-parser = argparse.ArgumentParser(description="A script to unhide posts on Reddit.")
-parser.add_argument("-u", "--username", help="Username to log into Reddit with.", action="store")
-parser.add_argument("-p", "--password", help="Password to log into Reddit with.", action="store")
-parser.add_argument("-d", "--days-to-keep", help="The number of days of posts to not unhide.", type=int, default=1, action="store")
-args = parser.parse_args()
+load_dotenv()
 
-if args.days_to_keep < 0:
-	args.days_to_keep = 0
+reddit = praw.Reddit(
+    client_id=os.environ["CLIENT_ID"],
+    client_secret=os.environ["CLIENT_SECRET"],
+    password=os.environ["PASSWORD"],
+    user_agent="Reddit Unhider",
+    username=os.environ["USERNAME"],
+)
 
-
-reddit = praw.Reddit(user_agent="Reddit-unhider")
-reddit.login(args.username, args.password)
-
-time_cutoff = int(time.time()) - (args.days_to_keep * 60 * 60 * 24)
 num_hidden = 0
-after = None
 no_more = False
 while not no_more:
-	hidden_posts = reddit.user.get_hidden(limit=None, params={"after" : after})
-	no_more = True
-	for post in hidden_posts:
-		no_more = False
-		if int(post.created_utc) >= time_cutoff:
-			after = post.name
-			continue
-		print("Unhiding [%s] ... " % post.title)
-		post.unhide()
-		num_hidden += 1
+    hidden_posts = reddit.user.me().hidden()
+    no_more = True
+    for post in hidden_posts:
+        no_more = False
+        print("Unhiding [%s] ... " % post.title)
+        post.unhide()
+        num_hidden += 1
 
 print("\n%d posts unhidden!" % num_hidden)
